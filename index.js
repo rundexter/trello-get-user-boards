267574e97d917ec5c3f55aaea4b0978d9636e2ab
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-24 15:34:28 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
+var trello = require('node-trello'),
+    _ = require('lodash'),
+    util = require('./util'),
+    pickInputs = {
+        idMember: 'idMember',
+        username: 'username'
+    },
+    pickOutputs = {
+        '-': {
+            keyName: 'data',
+            fields: {
+                id: 'id',
+                name: 'name',
+                memberships: 'memberships',
+                shortLink: 'shortLink',
+                shortUrl: 'shortUrl',
+                starred: 'starred',
+                subscribed: 'subscribed',
+                url: 'url'
+            }
+        }
+    };
+
 module.exports = {
+    authOptions: function (dexter) {
+        if (!dexter.environment('trello_api_key') || !dexter.environment('trello_token')) {
+            this.fail('A [trello_api_key] or [trello_token] environment variables are required for this module');
+            return false;
+        } else {
+            return {
+                api_key: dexter.environment('trello_api_key'),
+                token: dexter.environment('trello_token')
+            }
+        }
+    },
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +40,20 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var auth = this.authOptions(dexter);
+        if (!auth) return;
+        var t = new trello(auth.api_key, auth.token);
+        var inputs = util.pickStringInputs(step, pickInputs);
+        var member = inputs.idMember? inputs.idMember : inputs.username;
+        if (_.isEmpty(member))
+            return this.fail('A [idMember] or [username] variables are required for this module');
+
+        t.get("/1/members/" + member + "/boards", function(err, data) {
+            if (!err) {
+                this.complete(util.pickResult({data: data}, pickOutputs));
+            } else {
+                this.fail(err);
+            }
+        }.bind(this));
     }
 };
